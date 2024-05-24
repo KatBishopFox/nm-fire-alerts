4563138c0f856a0b30dce543deb0ce7339f7d12f
Kat <98344291+KatBishopFox@users.noreply.github.com>
2024-05-24 10:31:01 -0400

    Added missing comma

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Katie Ritchie",
     author_email="ritchieke@gmail.com",
     description="Query the https://nifc.maps.arcgis.com/apps/dashboards/aa9ff369dd414b74b69b696b40a1d057 map to determine if New Mexico National Forests have fire prohibitions or bans.",
-    url="https://github.com/KatBishopFox/nm-fire-alerts"
+    url="https://github.com/KatBishopFox/nm-fire-alerts",
     packages=find_packages(exclude=("tests", "docs")),
     install_requires=requirements,
     classifiers=[
